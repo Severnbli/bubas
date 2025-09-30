b1ae80658a361730e15cb6fd9952d4285e3460ff
Usevalad Buben <usevaladbuben@gmail.com>
2025-09-30 17:52:39 +0300

    Add DbUtils helper for database connection string
    Introduces a static DbUtils class with a method to generate a database connection string using environment variables. This utility centralizes connection string creation for easier reuse and maintenance.

---
+++ Source/Shared/Utils/DbUtils.cs
@@ -0,0 +1,15 @@
+﻿using bubas.Source.Shared.Constants;
+using DotNetEnv;
+
+namespace bubas.Source.Shared.Utils;
+
+public static class DbUtils
+{
+    public static string GetDbConnectionString()
+    {
+        var dbName = Env.GetString(Destination.DbName);
+        var connectionString = $"Data Source={dbName}";
+        
+        return connectionString;
+    }
+}
