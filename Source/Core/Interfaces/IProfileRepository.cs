7adddd4c89a525c559e828401eec978a07adf3bd
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-01 00:08:51 +0300

    Add repository interfaces and update BotDbContext
    Introduced generic IRepository and specific repository interfaces for Profile, ProfileAnnouncement, StudentSchedule, and WeatherData entities. Updated BotDbContext to include DbSet properties for these entities. Also removed redundant folder entry from the project file.

---
+++ Source/Core/Interfaces/IProfileRepository.cs
@@ -0,0 +1,8 @@
+﻿using bubas.Source.Core.Entities;
+
+namespace bubas.Source.Core.Interfaces;
+
+public interface IProfileRepository : IRepository<Profile>
+{
+    public Task<Profile?> GetByChatId(long chatId);
+}
--- Source/Infrastructure/Data/BotDbContext.cs
+++ Source/Infrastructure/Data/BotDbContext.cs
@@ -1,11 +1,16 @@
-﻿using bubas.Source.Shared.Utils;
-using DotNetEnv;
+﻿using bubas.Source.Core.Entities;
+using bubas.Source.Shared.Utils;
 using Microsoft.EntityFrameworkCore;
 
 namespace bubas.Source.Infrastructure.Data;
 
 public sealed class BotDbContext : DbContext
 {
+    public DbSet<Profile> Profiles { get; set; }
+    public DbSet<ProfileAnnouncement> ProfileAnnouncements { get; set; }
+    public DbSet<WeatherData> WeatherData { get; set; }
+    public DbSet<StudentSchedule> StudentSchedules { get; set; }
+    
     public BotDbContext()
     {
         Database.EnsureDeleted();
