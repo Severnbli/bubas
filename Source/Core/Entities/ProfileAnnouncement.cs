2c0e21b8f2f00f86643b6b0b45b3e1d3adde53ad
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-01 00:07:29 +0300

    Add core entity classes for profile management
    Introduced new entity classes: Profile, ProfileAnnouncement, StudentSchedule, and WeatherData in the Core/Entities directory to support profile-related features. Updated the project file to reflect the new structure.

---
+++ Source/Core/Entities/ProfileAnnouncement.cs
@@ -0,0 +1,9 @@
+﻿namespace bubas.Source.Core.Entities;
+
+public class ProfileAnnouncement
+{
+    public long Id { get; set; }
+    public TimeOnly TimeOfAnnouncement { get; set; }
+    public int UtcFactor { get; set; }
+    public bool IsEnabled { get; set; }
+}
---
+++ Source/Core/Entities/WeatherData.cs
@@ -0,0 +1,9 @@
+﻿using bubas.Source.Core.Enums;
+
+namespace bubas.Source.Core.Entities;
+
+public class WeatherData
+{
+    public long Id { get; set; }
+    public WeatherProviders WeatherProvider { get; set; } = WeatherProviders.OpenWeatherMap;
+}
