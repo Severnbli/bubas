55b14369faa7b31bc089b7bc0da42af7de19edc0
Usevalad Buben <usevaladbuben@gmail.com>
2025-09-30 17:53:02 +0300

    Add BotDbContext for SQLite database integration
    Introduces BotDbContext, a sealed DbContext class that configures a SQLite database using a connection string from DbUtils. The context ensures the database is deleted and recreated on initialization. Also updates the project file to remove some unused folder includes.

---
+++ Source/Infrastructure/Data/BotDbContext.cs
@@ -0,0 +1,19 @@
+﻿using bubas.Source.Shared.Utils;
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+
+namespace bubas.Source.Infrastructure.Data;
+
+public sealed class BotDbContext : DbContext
+{
+    public BotDbContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(DbUtils.GetDbConnectionString());
+    }
+}
