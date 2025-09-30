ced3cd6705f717347b4ac847904227a984eb222b
Usevalad Buben <usevaladbuben@gmail.com>
2025-09-30 17:52:14 +0300

    Add Destination constants class
    Introduces a static class 'Destination' with constants for environment file path and database name.

---
+++ Source/Shared/Constants/Destination.cs
@@ -0,0 +1,7 @@
+﻿namespace bubas.Source.Shared.Constants;
+
+public static class Destination
+{
+    public static readonly string EnvFilePath = ".env";
+    public static readonly string DbName = "DB_NAME";
+}
