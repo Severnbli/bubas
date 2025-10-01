300ab56b5167e344dc50d8ed208c5bbd7f458158
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-01 17:39:03 +0300

    Add BaseDiInstaller abstract class for DI setup
    Introduces BaseDiInstaller, an abstract class implementing IDiInstaller, to standardize dependency injection container setup using Autofac. Removes redundant folder entry from the project file.

---
+++ Source/Bootstrap/Installers/BaseDiInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using bubas.Source.Core.Interfaces;
+
+namespace bubas.Source.Bootstrap.Installers;
+
+public abstract class BaseDiInstaller : IDiInstaller
+{
+    public IContainer InstallDiContainer()
+    {
+        var builder = new ContainerBuilder();
+        RegisterModules(builder);
+        
+        var container = builder.Build(); 
+        
+        return container;
+    }
+    
+    protected abstract void RegisterModules(ContainerBuilder builder);
+}
