5cc1ff8d3b571a15bb1ca849e6c23f2c3ee8d46b
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-01 18:06:38 +0300

    Add DefaultBotStarter for Telegram bot initialization
    Introduces DefaultBotStarter class to handle bot building, command upload, and startup. Removes redundant folder entry from the project file.

---
+++ Source/TelegramBot/Starters/DefaultBotStarter.cs
@@ -0,0 +1,15 @@
+﻿using bubas.Source.Core.Interfaces;
+
+namespace bubas.Source.TelegramBot.Starters;
+
+public class DefaultBotStarter(IBotBuilder builder) : IBotStarter
+{
+    public async Task StartBot()
+    {
+        var bot = builder.BuildBot();
+        await bot.UploadBotCommands();
+        await bot.Start();
+
+        await Task.Delay(-1);
+    }
+}
