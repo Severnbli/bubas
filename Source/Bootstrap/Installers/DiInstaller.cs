51ee4989e9ec7b05e5b13e097017d92bdd75ec23
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-01 17:39:24 +0300

    Add DiInstaller for registering Autofac modules
    Introduces DiInstaller class that registers the DbModule with Autofac's ContainerBuilder, extending BaseDiInstaller.

---
+++ Source/Bootstrap/Installers/DiInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+using bubas.Source.Bootstrap.Modules;
+
+namespace bubas.Source.Bootstrap.Installers;
+
+public class DiInstaller : BaseDiInstaller
+{
+    protected override void RegisterModules(ContainerBuilder builder)
+    {
+        builder.RegisterModule<DbModule>();
+    }
+}
