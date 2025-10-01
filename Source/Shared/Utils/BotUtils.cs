dda55bfa366e8a3d781714b40689f9f16a14dde4
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-01 17:49:12 +0300

    Add BotApi constant and BotUtils helper
    Introduced a new BotApi constant in Destination.cs and added BotUtils.cs with a utility method to retrieve the bot API key from environment variables.

---
+++ Source/Shared/Utils/BotUtils.cs
@@ -0,0 +1,13 @@
+﻿using bubas.Source.Shared.Constants;
+using DotNetEnv;
+
+namespace bubas.Source.Shared.Utils;
+
+public static class BotUtils
+{
+    public static string GetBotApiKey()
+    {
+        var apiKey = Env.GetString(Destination.BotApi);
+        return apiKey;
+    }
+}
