582d7490b2b6d123dbce2434347a506e1e966748
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-02 11:18:17 +0300

    Add Render method to StartForm
    Implemented the Render method in StartForm to send a 'Hello World!' message when invoked.

--- Source/TelegramBot/Forms/StartForm.cs
+++ Source/TelegramBot/Forms/StartForm.cs
@@ -1,8 +1,14 @@
-﻿using TelegramBotBase.Form;
+﻿using TelegramBotBase.Base;
+using TelegramBotBase.Form;
 
 namespace bubas.Source.TelegramBot.Forms;
 
 public class StartForm : FormBase
 {
-    
+    public override Task Render(MessageResult message)
+    {
+        Device.Send("Hello World!");
+        
+        return Task.CompletedTask;
+    }
 }
