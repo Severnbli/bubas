eed50e3001b6b8f94fc69306aa7717bc6be88c5b
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-03 00:07:42 +0300

    Refactor StartForm to use AutoCleanForm and add navigation
    StartForm now inherits from AutoCleanForm, enabling automatic message deletion on form leave. Added Action method to handle callback data and navigate to MainMenuForm when 'letsgo' is selected. Updated Render to display a localized greeting and a button with callback data.

--- Source/TelegramBot/Forms/StartForm.cs
+++ Source/TelegramBot/Forms/StartForm.cs
@@ -1,13 +1,49 @@
 ﻿using TelegramBotBase.Base;
+using TelegramBotBase.DependencyInjection;
+using TelegramBotBase.Enums;
 using TelegramBotBase.Form;
 
 namespace bubas.Source.TelegramBot.Forms;
 
-public class StartForm : FormBase
+public class StartForm : AutoCleanForm
 {
+    public StartForm()
+    {
+        DeleteMode = EDeleteMode.OnLeavingForm;
+        DeleteSide = EDeleteSide.BotOnly;
+    }
+
+    public override async Task Action(MessageResult message)
+    {
+        var call = message.GetData<CallbackData>();
+        await message.ConfirmAction();
+
+        if (call == null) return;
+        
+        message.Handled = true;
+
+        switch (call.Value)
+        {
+            case "letsgo":
+            {
+                await this.NavigateTo<MainMenuForm>();
+                break;
+            }
+            default:
+            {
+                message.Handled = false;
+                break;
+            }
+        }
+    }
+    
     public override Task Render(MessageResult message)
     {
-        Device.Send("Hello World!");
+        var btn = new ButtonForm();
+        
+        btn.AddButtonRow(new ButtonBase("За справу!", new CallbackData("a", "letsgo").Serialize()));
+        
+        Device.Send("Прывітанне, сябрук! ⚡ Усё гатова для старту – за табой чарга!", btn);
         
         return Task.CompletedTask;
     }
