955e05ed9129a8bd87e841686f5ee309dc63abcb
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-02 11:18:08 +0300

    Refactor Program.cs to modularize startup logic
    Split the Main method into smaller methods: LoadEnvs, SetupContainer, and StartBot for better readability and maintainability. Environment variables are now loaded from a specified file before DI container setup.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Autofac;
 using bubas.Source.Bootstrap.Installers;
 using bubas.Source.Core.Interfaces;
+using bubas.Source.Shared.Constants;
+using DotNetEnv;
 
 namespace bubas;
 
@@ -8,9 +10,26 @@ internal static class Program
 {
     private static async Task Main(string[] args)
     {
+        LoadEnvs();
+        var container = SetupContainer();
+        await StartBot(container);
+    }
+
+    private static void LoadEnvs()
+    {
+        Env.Load(Destination.EnvFilePath);
+    }
+
+    private static IContainer SetupContainer()
+    {
         IDiInstaller diInstaller = new DefaultDiInstaller();
         var container = diInstaller.InstallDiContainer();
+        
+        return container;
+    }
 
+    private static async Task StartBot(IContainer container)
+    {
         var botStarter = container.Resolve<IBotStarter>();
         await botStarter.StartBot();
     }
