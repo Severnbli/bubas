a453195823ef31f872ef5c46303f90a90fb7e5fe
Usevalad Buben <usevaladbuben@gmail.com>
2025-10-03 00:07:05 +0300

    Add BotExtensions with AddDefaultCommands method
    Introduces a static extension class for BotBase, providing an AddDefaultCommands method that registers a default command handler to navigate to StartForm for unrecognized commands.

---
+++ Source/Shared/Extensions/BotExtensions.cs
@@ -0,0 +1,23 @@
+﻿using bubas.Source.TelegramBot.Forms;
+using TelegramBotBase;
+using TelegramBotBase.DependencyInjection;
+
+namespace bubas.Source.Shared.Extensions;
+
+public static class BotExtensions
+{
+    public static void AddDefaultCommands(this BotBase bot)
+    {
+        bot.BotCommand += async (sender, args) =>
+        {
+            switch (args.Command)
+            {
+                default:
+                {
+                    await args.Device.ActiveForm.NavigateTo<StartForm>();
+                    break;
+                }
+            }
+        };
+    }
+}
